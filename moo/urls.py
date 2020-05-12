84fce84693bf76b9f72de9fcb4b27e9bd08d7d4b
Janell.Huyck <Janell.Huyck@outlook.com>
2020-05-12 15:58:07 -0400

    changed background, arranged elements visually, and put in guard against apostrophes.

--- moo/urls.py
+++ moo/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from moo import views
 
 urlpatterns = [path('', views.index, name='home'),
-               path('moolist', views.moolist, name="moolist")]
+               path('history', views.history, name="history")]
--- moo/views.py
+++ moo/views.py
@@ -13,6 +13,18 @@ def index(request):
         form = MooForm(request.POST)
         if form.is_valid():
             words = form.cleaned_data['text']
+            if "'" in words:
+                form = MooForm()
+                message_before = """Sorry, cows can't speak with apostrophes.
+                  Try again?"""
+                context = {
+                    'form': form,
+                    'message_before': message_before,
+                    'cow_list': cow_list,
+                    'html': html,
+                }
+                return render(request, 'index.html', context)
+
             cow_byte_string = get_cow.get_cow(words)
             cow_list = cow_byte_string.decode()
             MooText.objects.create(
@@ -28,10 +40,10 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def moolist(request):
+def history(request):
     moos = []
     all_moos = MooText.objects.all().order_by('-pk')
     for i in range(0, 10):
         moos.append([i + 1, all_moos[i].text])
-    return render(request, 'moo_list.html', {
+    return render(request, 'history.html', {
         'moos': moos, })
